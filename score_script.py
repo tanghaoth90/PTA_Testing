0e5c7cd7fe6b92ceb5460ab2de0b8005322a40c6
Hao Tang <tanghaoth90@gmail.com>
2018-08-06 22:57:03 +0800

    python for one user (without scoring)

--- score_script.py
+++ score_script.py
@@ -1,14 +1,28 @@
+import subprocess, os
+
+def get_result(filename):
+	res = {}
+	with open(filename, "r") as answer:
+		for row in answer:
+			row = row.strip()
+			if not row: continue
+			try:
+				pt, objs = [t.strip() for t in row.split(":")]
+				objs = set([int(t) for t in objs.split()])
+				res[int(pt)] = objs
+			except:
+				pass
+	return res
+
 with open("resources/benchmark_info.txt", "r") as benchmark_info_file:
 	for benchmark_name in benchmark_info_file:
 		benchmark_name = benchmark_name.strip()
-		if benchmark_name:	
-			print "BM[%s]" % benchmark_name
-			with open("resources/standardOutput/%s.stdout"%benchmark_name, "r") as standardOutput:
-				std_dict = {}
-				for row in standardOutput:
-					row = row.strip()
-					if row:
-						pt, objs = [t.strip() for t in row.split(":")]
-						objs = set([int(t) for t in objs.split()])
-						std_dict[int(pt)] = objs
-				print std_dict
+		if not benchmark_name: continue
+		abbr_benchmark_name = benchmark_name.split(".")[-1]
+		print("BM [%s]" % abbr_benchmark_name)
+		stdAns = get_result("resources/standardAnswer/%s.stdout"%abbr_benchmark_name)
+		print(stdAns)
+		if os.path.isfile("result.txt"): os.remove("result.txt")
+		subprocess.run(["java", "-jar", "analyzer.jar", "resources/finaltest", benchmark_name])
+		userAns = get_result("result.txt")
+		print(userAns)
